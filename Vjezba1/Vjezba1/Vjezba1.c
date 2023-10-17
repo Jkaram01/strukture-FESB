426891ac46c38671f92e64dd9d7051a104c855cb
Jkaram01 <147596294+Jkaram01@users.noreply.github.com>
2023-10-17 22:35:11 +0200

    Update Vjezba1.c

--- Vjezba1/Vjezba1/Vjezba1.c
+++ Vjezba1/Vjezba1/Vjezba1.c
@@ -1,8 +1,24 @@
-#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
 int main() {
-	
-	printf("Hello World!!!\n");
+    int count = 0;
+    char ch;
+    FILE* file = fopen("studenti.txt", "r");
 
-	return 0;
+    if (file == NULL) {
+        printf("ERROR 1\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == '\n') {
+            count++;
+        }
+    }
+
+    fclose(file);
+
+    printf("Number of students in the file: %d\n", count);
+    return 0;
 }
